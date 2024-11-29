3847cea6fdd7ec5e0f0015fa1a67b53e3caa21f0
FrejaMeyer <Frejme@gmail.com>
2024-11-29 13:20:54 +0100

    Resterende Query & Command, og meget andet.

--- MyFeedback.Domain/Entities/Comment.cs
+++ MyFeedback.Domain/Entities/Comment.cs
@@ -12,5 +12,32 @@ namespace MyFeedback.Domain.Entities
         public DateTime CommentDate { get; protected set; }
         public List<PastComment> PastComments { get; protected set; }
         public Guid IdentityUserId { get; protected set; }
+
+        public Comment()
+        {
+            
+        }
+
+        public Comment(string commentText, DateTime commentDate, List<PastComment> pastComments, Guid identityUserId)
+        {
+            CommentText = commentText;
+            CommentDate = commentDate;
+            PastComments = pastComments;
+            IdentityUserId = identityUserId;
+        }
+
+        public static Comment Create(string commentText, DateTime commentDate, List<PastComment> pastComments, Guid identityUserId)
+        {
+            Comment newComment = new Comment(commentText, commentDate, pastComments, identityUserId);
+            return newComment;
+        }
+
+        public void Update(string commentText, DateTime commentDate, List<PastComment> pastComments, Guid identityUserId)
+        {
+            CommentText = commentText;
+            CommentDate = commentDate;
+            PastComments = pastComments;
+            IdentityUserId = identityUserId;
+        }
     }
 }
